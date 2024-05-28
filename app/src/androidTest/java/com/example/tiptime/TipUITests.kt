7c2641a60f27cceb9adefe25e8df1d17844691e7
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-28 09:58:38 -0600

    chore: Add notes about instrumented test

--- app/src/androidTest/java/com/example/tiptime/TipUITests.kt
+++ app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -2,9 +2,21 @@ package com.example.tiptime
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
+import org.junit.Test
 
 class TipUITests {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        // you always have to set content similar to how UI is set in "onCreate" method in an Activity
+        // as instrumented tests test an actual instance of the app and its UI. Remember all instrumented
+        // tests are built into its own "APK" which is then installed in an Emulator or device along
+        // with the regular app APK and then the "test APK" runs its tests against the "app APK"
+        composeTestRule.setContent {
+
+        }
+    }
 }
