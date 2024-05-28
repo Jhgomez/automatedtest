d7f622a23694ac3e3072b00428119c117f8b0dd1
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-28 10:39:18 -0600

    chore: format expected tip amount, find compose node and assert it exists to evaluate test

--- app/src/androidTest/java/com/example/tiptime/TipUITests.kt
+++ app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -1,5 +1,6 @@
 package com.example.tiptime
 
+import android.icu.text.NumberFormat
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
@@ -28,5 +29,11 @@ class TipUITests {
 
         composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
         composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+
+        composeTestRule
+            .onNodeWithText("Tip Amount: $expectedTip")
+            .assertExists("No node with this text was found")
     }
 }
