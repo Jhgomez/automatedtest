4b304fb1f2c547ca167c9c88280560e0bb2abb3d
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-27 23:32:26 -0600

    chore: Add test to check calculate tip method

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -1,4 +1,19 @@
 package com.example.tiptime
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
 class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundUp() {
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val amount = 10.0
+        val tipPercent = 20.0
+
+        val actualTip = calculateTip(amount, tipPercent, false)
+
+        assertEquals(expectedTip, actualTip)
+    }
 }
