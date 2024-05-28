c4173a6c0935f2c7480a55f57ca315766a07a7e7
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-28 10:00:45 -0600

    chore: Add note about setting content

--- app/src/androidTest/java/com/example/tiptime/TipUITests.kt
+++ app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -9,12 +9,13 @@ class TipUITests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    // you always have to set content similar to how UI is set in "onCreate" method in an Activity
+    // as instrumented tests test an actual instance of the app and its UI. Remember all instrumented
+    // tests are built into its own "APK" which is then installed in an Emulator or device along
+    // with the regular app APK and then the "test APK" runs its tests against the "app APK"
     @Test
     fun calculate_20_percent_tip() {
-        // you always have to set content similar to how UI is set in "onCreate" method in an Activity
-        // as instrumented tests test an actual instance of the app and its UI. Remember all instrumented
-        // tests are built into its own "APK" which is then installed in an Emulator or device along
-        // with the regular app APK and then the "test APK" runs its tests against the "app APK"
+        // As you can see this sets the UI content of the compose test rule
         composeTestRule.setContent {
 
         }
